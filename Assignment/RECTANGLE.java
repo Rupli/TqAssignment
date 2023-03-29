2a759a23264b2332980ed14f21e3a2798ac2c6a6
Administrator <Administrator@DESKTOP-FIK5SPH>
2023-03-28 23:19:52 -0700

    Socond

---
+++ Assignment/RECTANGLE.java
@@ -0,0 +1,20 @@
+// wap length and bredth rectange find perimeter==>2(L+B) L+L+B+B
+class RECTANGLE
+public static void main(String args[]){
+
+int length1=12;
+
+int length2=12;
+
+int Breadth1=7;
+
+int Breadth2=7;
+
+int perimeter=length1+length2+Breadth1+Breadth2;
+
+System.out.println("Perimeter of rectangle" + perimeter);
+
+
+}
+
+}
